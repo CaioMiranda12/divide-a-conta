46b13690abfe3983ddd7d011f2235081eaf7917e
CaioMiranda12 <caioclm04@gmail.com>
2026-08-02 21:38:47 -0300

    feat: add auth constants and types

---
+++ src/constants/auth.ts
@@ -0,0 +1,6 @@
+const SECONDS_PER_DAY = 60 * 60 * 24;
+
+export const SESSION_DURATION_IN_SECONDS = SECONDS_PER_DAY * 7;
+export const SESSION_COOKIE_NAME = 'divide_a_conta_session';
+export const PASSWORD_HASH_SALT_ROUNDS = 10;
+export const MIN_PASSWORD_LENGTH = 8;
---
+++ src/types/auth.ts
@@ -0,0 +1,9 @@
+export type SessionPayload = {
+  userId: string;
+};
+
+export type AuthenticatedUser = {
+  id: string;
+  name: string;
+  email: string;
+};
